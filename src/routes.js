15d1e77ef978c29d7034a579fc4b77e90f168642
Brayan Vinicius Jordan <brayan.11fera@gmail.com>
2022-02-28 13:27:02 -0300

    Query um pouco mais complexa com Node

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const UserController = require('./controllers/UserController');
 const AddressController = require('./controllers/AddressController');
 const TechController = require('./controllers/TechController');
+const ReportController = require('./controllers/ReportController');
 
 //consign
 
@@ -17,5 +18,7 @@ routes.post('/users/:user_id/addresses', AddressController.store);
 routes.get('/users/:user_id/techs', TechController.index);
 routes.post('/users/:user_id/techs', TechController.store);
 routes.delete('/users/:user_id/techs', TechController.delete);
+
+routes.get('/report', ReportController.show)
 
 module.exports = routes;
